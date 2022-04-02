88c72361428625e87ad388ae2698f6af79ddd3a3
joaocastr <joaovictorc2506@gmail.com>
2022-04-02 11:30:16 -0300

    uppdate4

--- Assets/scripts/jogador.cs
+++ Assets/scripts/jogador.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class jogador : MonoBehaviour
-{
+{   
+    public float velocidade = 5.0f;
+    public float rotacao = 2.0f;
+    public GameObject prefabMeteoro;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,21 +17,27 @@ public class jogador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = transform.position + new Vector3(0.05f, 0.0f, 0.0f);
-        }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position = transform.position + new Vector3(0.0f, 0.05f, 0.0f);
+            transform.position = transform.position + new Vector3(0.0f, velocidade * Time.deltaTime , 0.0f);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position = transform.position + new Vector3(0.0f, -0.05f, 0.0f);
+            transform.position = transform.position + new Vector3(0.0f, -velocidade * Time.deltaTime , 0.0f);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = transform.position + new Vector3(-0.05f, 0.0f, 0.0f);
+            transform.position = transform.position + new Vector3(-velocidade * Time.deltaTime , 0.0f, 0.0f);
+            transform.Rotate(0.0f, 0.0f, 2.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(velocidade * Time.deltaTime, 0.0f, 0.0f);
+            transform.Rotate(0.0f, 0.0f, -2.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate( prefabMeteoro, new Vector3 (4.0f,4.0f,0.0f), Quaternion.identity);
         }
     }
 }
